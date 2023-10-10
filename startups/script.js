127e49308be9b5b1f60a3e74921daaca0abe1c85
Phil Wee <pkw2010@nyu.edu>
2023-10-10 17:28:40 +0400

    Update script.js

--- startups/script.js
+++ startups/script.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
             'Authorization': `Bearer ${process.env.REACT_APP_SHEETSON_API_KEY}`,
             'X-Sheetson-Spreadsheet-Id': process.env.REACT_APP_SHEETSON_SPREADSHEET_ID }
     })
+    
     .then(r => r.json())
     .then(data => {
         var template = Handlebars.compile($('#startup-template').html());
